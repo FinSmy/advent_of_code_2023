727dd12d9475ddd8be51a8427f2f3f6da736c740
FinSmy <finadamsmyth@gmail.com>
2024-10-28 22:10:48 +0000

    Day 3 complete

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,72 +1,150 @@
-use std::fs;
+use itertools::Itertools;
 use regex::Regex;
+use std::collections::{HashMap, HashSet};
+use std::fs;
+use thiserror::Error;
 
-pub struct Map {
-    map: Vec<Vec<char>>,
-    width: usize,
-    height: usize,   
+#[derive(Error, Debug, PartialEq, Eq)]
+enum PuzzleErr {
+    #[error("Could not parse game: '{}'.", .0)]
+    GameParsingError(String),
 }
 
-fn parse_to_map(file: &str) -> Map {
-    let mut map: Vec<Vec<char>> = Vec::new();
-    let mut width: usize = 0;
-    let mut height: usize = 0;
-    for line in file.lines() {
-        let mut row: Vec<char> = Vec::new();
-        for c in line.chars() {
-            row.push(c);
-        }
-        width = row.len();
-        map.push(row);
-        height += 1;
-    }
-    
-    Map {
-        map,
-        width,
-        height,
-    }
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Coord {
+    r: i32,
+    c: i32,
 }
 
-fn solve(map: Map) {
-    for y in 0..map.height-1 {
-        println!("{:?}", map.map[y]);
-        for x in 0..map.width-1 {
-            let c = map.map[y][x];
+#[derive(Debug, Clone)]
+struct PartNum {
+    val: i32,
+    locs: Vec<Coord>,
+}
 
-            // Find full part number
-            if c.is_digit(10) {
-                let mut curr_part = c.to_digit(10).unwrap();
-                
-                let mut look_ahead: usize;
-                if x < map.width{
-                    look_ahead = 1;
-                } else {
-                    look_ahead = 0;
-                }
-                while map.map[y][x+look_ahead].is_digit(10){
-                    let add = map.map[y][x+look_ahead].to_digit(10).unwrap();
-                    curr_part = curr_part*10 + add;
-                    if x+look_ahead+1 < map.width {
-                        look_ahead += 1;
-                    } else {
-                        break;
+impl PartNum {
+    fn is_near_symbol(&self, symbols: &HashSet<Coord>) -> bool {
+        for coord in self.locs.iter() {
+            for dr in -1..=1 {
+                for dc in -1..=1 {
+                    let neighbour = Coord {
+                        r: coord.r + dr,
+                        c: coord.c + dc,
+                    };
+                    if symbols.contains(&neighbour) {
+                        return true;
                     }
-                }
-                
-                // Check for symbol above and below
-                if (y > 0) && (y < map.height) {
-                    let row_above = y - 1;
-                    let row_below = y + 1;
-                }
-
-                if (x > 0) && (x < map.width) {
-                    let left = x - 1;
-                    let right = x + 1;
                 }
             }
         }
+        false
     }
+
+    fn from_components(part_num_comps: &[(Coord, char)]) -> Self {
+        let val = part_num_comps
+            .iter()
+            .map(|(_, c)| c)
+            .join("")
+            .parse::<i32>()
+            .unwrap();
+        PartNum {
+            val,
+            locs: part_num_comps.iter().map(|(c, _)| c).cloned().collect(),
+        }
+    }
+}
+
+fn extract_part_nums_from_line(line: &str, row: &i32) -> Result<Vec<PartNum>, PuzzleErr> {
+    let mut pns = Vec::new();
+    let digit_re = Regex::new(r"\d").unwrap();
+    let mut pn_comps: Vec<(Coord, char)> = Vec::new();
+    for (i, c) in line.chars().enumerate() {
+        if digit_re.is_match(&c.to_string()) {
+            pn_comps.push((
+                Coord {
+                    r: *row,
+                    c: i as i32,
+                },
+                c,
+            ))
+        } else if !pn_comps.is_empty() {
+            pns.push(PartNum::from_components(&pn_comps));
+            pn_comps = Vec::new();
+        }
+    }
+
+    if !pn_comps.is_empty() {
+        pns.push(PartNum::from_components(&pn_comps));
+    }
+
+    Ok(pns)
+}
+
+fn extract_symbols(input: &str) -> Result<HashMap<Coord, char>, PuzzleErr> {
+    let mut symbols = HashMap::new();
+    let digit_regex = Regex::new(r"\d").unwrap();
+    for (r, line) in input.trim().lines().enumerate() {
+        for (c, sym) in line.trim().chars().enumerate() {
+            if (sym == '.') | (digit_regex.is_match(&sym.to_string())) {
+                continue;
+            }
+            symbols.insert(
+                Coord {
+                    r: r as i32,
+                    c: c as i32,
+                },
+                sym,
+            );
+        }
+    }
+    Ok(symbols)
+}
+
+fn parse_input(input: &str) -> Result<(Vec<PartNum>, HashMap<Coord, char>), PuzzleErr> {
+    let part_nums = input
+        .trim()
+        .lines()
+        .enumerate()
+        .map(|(r, txt)| extract_part_nums_from_line(txt.trim(), &(r as i32)))
+        .flatten_ok()
+        .collect::<Result<Vec<_>, PuzzleErr>>()?;
+    let symbols = extract_symbols(input)?;
+    Ok((part_nums, symbols))
+}
+
+fn puzzle_1(input: &str) -> Result<i32, PuzzleErr> {
+    let (part_nums, syms) = parse_input(input)?;
+    let syms = HashSet::from_iter(syms.keys().cloned());
+    Ok(part_nums
+        .iter()
+        .filter(|p| p.is_near_symbol(&syms))
+        .map(|p| p.val)
+        .sum())
+}
+
+fn get_neighbors(coord: &Coord, part_nums: &[PartNum]) -> Vec<PartNum> {
+    let syms = HashSet::from_iter([*coord]);
+    part_nums
+        .iter()
+        .filter(|p| p.is_near_symbol(&syms))
+        .cloned()
+        .collect()
+}
+
+pub fn puzzle_2(input: &str) -> Result<i32, PuzzleErr> {
+    let (part_nums, symbols) = parse_input(input)?;
+    Ok(symbols
+        .iter()
+        .filter(|(_, s)| s == &&'*')
+        .map(|(c, _)| {
+            let pn = get_neighbors(c, &part_nums);
+            if pn.len() == 2 {
+                pn[0].val * pn[1].val
+            } else {
+                0
+            }
+        })
+        .sum())
 }
 
 fn main() {
@@ -74,7 +152,15 @@ fn main() {
 
     let file = fs::read_to_string("./input.txt").unwrap();
 
-    let engine_map = parse_to_map(&file);
+    let answer_1 = puzzle_1(&file);
+    match answer_1 {
+        Ok(x) => println!("Puzzle 1 answer {}", x),
+        Err(er) => panic!("No solution found for puzzle_1: {}.", er),
+    }
 
-    solve(engine_map);
+    let answer_2 = puzzle_2(&file);
+    match answer_2 {
+        Ok(x) => println!("Puzzle 2 answer_2 {}", x),
+        Err(e) => panic!("No solution found for puzzle_2: {}.", e),
+    }
 }
