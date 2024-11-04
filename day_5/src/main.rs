7ad62529fa2c0a78127541b68796340f1c072577
Finlay Smyth <finlay.smyth@cambridgeconsultants.com>
2024-11-04 11:36:01 +0000

    Parsing progress day 5

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,19 +1,39 @@
-use std::fs;
 use regex::Regex;
+use std::fs;
 
-fn extract_seeds(lines: &str) {
+fn extract_numbers(lines: &str) {
+    let Some(numbers_re) = Regex::new(r"(\d+)").unwrap().captures(lines) else {
+        return;
+    };
 
-    let numbers_re = Regex::new(r"(\d+)"
-        .unwrap()
-        .captures(lines)
+    println!("{:?}", numbers_re.extract());
+
+    /*
+    for (_, substr) in numbers_re {
+        if digit_re.is_match(&c.to_string()) {
+            num_comps = num_comps * 10 + c.to_digit(10).unwrap();
+        } else if !(num_comps == 0) {
+            nums.push(num_comps);
+            num_comps = 0;
+        }
+    }
+    */
 }
 
-fn parse_input(input: &str) {
-    lines = input
+fn parse_input(input: &str) -> Vec<&str> {
+    let pattern: &str = "seeds";
+    let seeds: Vec<&str> = input
         .trim()
-        .lines();
+        .lines()
+        .filter(|&x| x.contains(pattern))
+        .collect();
+    seeds
+}
 
-    seeds = extract_seeds(lines);
+fn puzzle_1(input: &str) {
+    let seeds = parse_input(input);
+    println!("Seeds: {:?}", seeds);
+    extract_numbers(seeds[0]);
 }
 
 fn main() {
