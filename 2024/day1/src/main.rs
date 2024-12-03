da4875ed0bf2c648cf38b4398f4b9428efc9cbcd
FinSmy <finadamsmyth@gmail.com>
2024-12-03 22:57:48 +0000

    day1 wip

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,30 +1,37 @@
 use regex::Regex;
 use std::fs;
 
-fn parse_input(input: &str) -> Vec<&str> {
-    let lines: Vec<&str> = input.trim().lines().collect();
-    lines
-}
-
-fn extract_lists(lines: Vec<&str>) -> (Vec<i32>, Vec<i32>) {
-    let num_re = Regex::new(r"(\d)(\d)").unwrap();
+fn extract_lists(lines: &str) -> (Vec<i32>, Vec<i32>) {
+    let num_re = Regex::new(r"(?ms)(\d+)\s.*(\d+)").unwrap();
     let mut list1: Vec<i32> = Vec::new();
     let mut list2: Vec<i32> = Vec::new();
 
-    for line in lines {
-        let _ = num_re.captures_iter(line).map(|caps| {
-            let (_, [val1, val2]) = caps.extract();
-            list1.push(val1.parse::<i32>().unwrap());
-            list2.push(val2.parse::<i32>().unwrap());
-        });
+    let caps = num_re.captures_iter(lines);
+    for (_, [a, b]) in caps.map(|c| c.extract()) {
+        list1.push(a.parse::<i32>().unwrap());
+        list2.push(b.parse::<i32>().unwrap());
     }
 
     (list1, list2)
 }
 
+fn accumulate_diffs(v1: Vec<i32>, v2: Vec<i32>) {
+    let mut total_diffs: i32 = 0;
+
+    for i in 0..v1.len() {
+        let diff = v1[i] - v2[i];
+        total_diffs += diff.abs();
+    }
+
+    println!("Total difference is : {}", total_diffs);
+}
+
 fn puzzle_1(input: &str) {
-    let lines = parse_input(input);
-    let (list1, list2) = extract_lists(lines);
+    let (mut list1, mut list2) = extract_lists(input);
+    list1.sort();
+    list2.sort();
+
+    accumulate_diffs(list1, list2);
 }
 
 fn main() {
