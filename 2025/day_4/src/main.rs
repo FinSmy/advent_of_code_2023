818c3021009d32694aa93c94e4f49d1e815777e7
Finlay Smyth <finlay.smyth@cambridgeconsultants.com>
2025-12-05 16:57:41 +0000

    Progress on day4 part 2

--- 2025/day_4/src/main.rs
+++ 2025/day_4/src/main.rs
@@ -70,8 +70,58 @@ fn puzzle_1(input: &str) {
     println!("num_neighbour_rolls = {}", num_moveable_rolls);
 }
 
-// fn puzzle_2(input: &str) {
-// }
+fn puzzle_2(input: &str) {
+    let mut roll_map = parse_input(input);
+
+    let mut num_moveable_rolls = 0;
+    let mut num_moveable_rolls_last = -1;
+
+    while num_moveable_rolls != num_moveable_rolls_last {
+        for i in 1..roll_map.len() - 1 {
+            for j in 1..roll_map[0].len() - 1 {
+                if roll_map[i][j] == false {
+                    continue;
+                }
+                let mut num_neighbour_rolls = 0;
+                if roll_map[i-1][j-1] {
+                    num_neighbour_rolls += 1;
+                }
+                if roll_map[i-1][j] {
+                    num_neighbour_rolls += 1;
+                }
+                if roll_map[i-1][j+1] {
+                    num_neighbour_rolls += 1;
+                }
+
+                if roll_map[i][j-1] {
+                    num_neighbour_rolls += 1;
+                }
+                if roll_map[i][j+1] {
+                    num_neighbour_rolls += 1;
+                }
+
+                if roll_map[i+1][j-1] {
+                    num_neighbour_rolls += 1;
+                }
+                if roll_map[i+1][j] {
+                    num_neighbour_rolls += 1;
+                }
+                if roll_map[i+1][j+1] {
+                    num_neighbour_rolls += 1;
+                }
+
+                roll_map[i][j] = false;
+                // println!("i = {}, j = {}, num_neighbour_rolls = {}, valid = {}", i, j, num_neighbour_rolls, (num_neighbour_rolls < 4));
+                if num_neighbour_rolls < 4 {
+                    num_moveable_rolls += 1;
+                }
+            }
+            // println!(" ");
+        }
+        num_moveable_rolls_last = num_moveable_rolls
+    }
+    println!("num_neighbour_rolls = {}", num_moveable_rolls);
+}
 
 fn main() {
     println!("Day 4: Lobby");
@@ -79,5 +129,5 @@ fn main() {
     let file = fs::read_to_string("./input.txt").unwrap();
 
     puzzle_1(&file);
-    // puzzle_2(&file);
+    puzzle_2(&file);
 }
